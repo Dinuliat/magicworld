f05ff97cec6c378cab00130d6a7dc1d766abadf2
Dinuliat <dinuliat@mail.ru>
2024-05-19 16:59:19 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css"
+import {Magicworld} from "./magicworld";
+
+function App() {
+  return (
+    <div className="App vh-100">
+      <Magicworld/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/magicworld.tsx
@@ -0,0 +1,16 @@
+export const Magicworld = () =>{
+
+    return (
+        <div className="h-100 p-5">
+            <div className="MagicContainer h-100 w-100">
+                <div>поисковик</div>
+                <div>Волшебный подводный мир</div>
+                <div>Красного моря</div>
+                <div></div>
+
+                TEST
+            </div>
+        </div>
+
+    )
+}
